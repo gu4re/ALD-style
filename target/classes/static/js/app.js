f109717c6e5fe8a9aaa6f64f8b87fc7a0a6593f3
PoweeR02 <davidgc1911@gmail.com>
2023-03-28 12:54:29 +0200

    cambios en el login.html

--- target/classes/static/js/app.js
+++ target/classes/static/js/app.js
@@ -5,7 +5,13 @@
         var router = new Router([
             new Route('home', 'home.html', true),            
             new Route('about', 'about.html'),
-            new Route('popularItems','popularItems.html')
+            new Route('popularItems','popularItems.html'),
+            new Route('newArrivals','newArrivals.html'),
+            new Route('allProducts','allProducts.html'),
+            new Route('sales','sales.html'),
+            new Route('homeSec','homeSec.html'),
+            new Route('login','login.html'),
+            new Route('cart','cart.html')
         ]);
     }
     init();
