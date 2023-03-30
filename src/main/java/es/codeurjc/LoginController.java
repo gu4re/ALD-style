ca49c102f212653af7c68485cd5b26e985231b17
gu4re <99036688+gu4re@users.noreply.github.com>
2023-03-30 23:26:25 +0200

    posible login implementado

--- src/main/java/es/codeurjc/LoginController.java
+++ src/main/java/es/codeurjc/LoginController.java
@@ -1,6 +1,5 @@
 package es.codeurjc;
 
-import org.h2.util.json.JSONObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class LoginController {
-	@PostMapping("/login")
+	@PostMapping("#login")
 	public ResponseEntity<Void> login(@RequestBody String userRequested) {
 		//if (LoginService.authenticate(userRequested))
 			//return ResponseEntity.ok().build();
--- target/classes/static/js/login.js
+++ target/classes/static/js/login.js
@@ -1,29 +1,31 @@
-document.addEventListener("DOMContentLoaded", function() {
-        const form = document.querySelector('#login-form');
-        form.addEventListener('submit', (event) => {
-          event.preventDefault();
-          fetch('http://localhost:8080/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                email: document.getElementById('email').value,
-                password: document.getElementById('password').value
-            })
-          })
-          .then((response) => {
-            if(response.ok) {
-              // Si la respuesta es exitosa, redirigir al usuario al home
-              window.location.href = '#home';
-            } else {
-              // Si la respuesta indica un error de credenciales, mostrar un mensaje de alerta
-              //alert('Wrong credentials.');
-            }
-          })
-          .catch((error) => {
-            // Si ocurre un error durante la petición, mostrar un mensaje de alerta
-            alert('Unexpected error during authentication.');
-          });
-        });
+  const form = document.querySelector('#login-form');
+  console.log('form', form);
+  const btn = form.querySelector('#login-button');
+  btn.addEventListener('click', (event) => {
+      console.log('click');
+      event.preventDefault();
+      fetch('http://localhost:8080/login', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+          email: document.getElementById('email').value,
+          password: document.getElementById('password').value
+      })
+    })
+    .then((response) => {
+      console.log('response', response);
+      if(response.ok) {
+        // Si la respuesta es exitosa, redirigir al usuario al home
+        window.location.href = '#home';
+      } else {
+        // Si la respuesta indica un error de credenciales, mostrar un mensaje de alerta
+        alert('Wrong credentials.');
+      }
+    })
+    .catch((error) => {
+      // Si ocurre un error durante la petición, mostrar un mensaje de alerta
+      alert('Unexpected error during authentication.');
     });
+  });
