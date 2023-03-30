a2a990a84440043c5d396602b53b999e5543a0f6
gu4re <99036688+gu4re@users.noreply.github.com>
2023-03-30 16:53:09 +0200

    posible login implementado

--- src/main/java/es/codeurjc/LoginController.java
+++ src/main/java/es/codeurjc/LoginController.java
@@ -9,9 +9,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 @Controller
 public class LoginController {
 	@PostMapping("/login")
-	public ResponseEntity<Void> login(@RequestBody JSONObject userRequested) {
+	public ResponseEntity<Void> login(@RequestBody String userRequested) {
 		//if (LoginService.authenticate(userRequested))
 			//return ResponseEntity.ok().build();
-		return ResponseEntity.badRequest().build();
+		return ResponseEntity.ok().build();
 	}
 }
--- target/classes/static/js/login.js
+++ target/classes/static/js/login.js
@@ -1,29 +1,29 @@
-alert('Patata');
-const form = document.querySelector('#login');
-form.addEventListener('#button-login', (event) => {
-  alert('Patata');
-  event.preventDefault();
-  fetch('http://localhost:8080/login', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-        user: document.getElementById('user').value,
-        password: document.getElementById('password').value
-    })
-  })
-  .then((response) => {
-    if(response.ok) {
-      // Si la respuesta es exitosa, redirigir al usuario al home
-      window.location.href = '#home';
-    } else {
-      // Si la respuesta indica un error de credenciales, mostrar un mensaje de alerta
-      alert('Wrong credentials.');
-    }
-  })
-  .catch((error) => {
-    // Si ocurre un error durante la petición, mostrar un mensaje de alerta
-    alert('Unexpected error during authentication.');
-  });
-});
+document.addEventListener("DOMContentLoaded", function() {
+        const form = document.querySelector('#login-form');
+        form.addEventListener('submit', (event) => {
+          event.preventDefault();
+          fetch('http://localhost:8080/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: document.getElementById('email').value,
+                password: document.getElementById('password').value
+            })
+          })
+          .then((response) => {
+            if(response.ok) {
+              // Si la respuesta es exitosa, redirigir al usuario al home
+              window.location.href = '#home';
+            } else {
+              // Si la respuesta indica un error de credenciales, mostrar un mensaje de alerta
+              //alert('Wrong credentials.');
+            }
+          })
+          .catch((error) => {
+            // Si ocurre un error durante la petición, mostrar un mensaje de alerta
+            alert('Unexpected error during authentication.');
+          });
+        });
+    });
