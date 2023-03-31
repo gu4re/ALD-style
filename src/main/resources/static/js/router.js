fc875d7b00dbe04ada32720233316695500eb870
gu4re <99036688+gu4re@users.noreply.github.com>
2023-03-31 12:55:30 +0200

    intentando acoplar la pagina 404

--- src/main/resources/static/js/router.js
+++ src/main/resources/static/js/router.js
@@ -29,6 +29,7 @@ Router.prototype = {
         this.hasChanged(this, r);
     },
     hasChanged: function(scope, r){
+        var routeFound = true;
         if (window.location.hash.length > 0) {
             for (var i = 0, length = r.length; i < length; i++) {
                 var route = r[i];
@@ -38,20 +39,25 @@ Router.prototype = {
                       script.src = 'js/login.js?' + Date.now();
                       document.head.appendChild(script);
                     }
-                    scope.goToRoute(route.htmlName);
+                    scope.goToRoute(route.htmlName, !routeFound);
+                    break;
                 }
             }
+            scope.goToRoute('404.html', !routeFound);
         } else {
             for (var i = 0, length = r.length; i < length; i++) {
                 var route = r[i];
                 if(route.default) {
-                    scope.goToRoute(route.htmlName);
+                    scope.goToRoute(route.htmlName, !routeFound);
                 }
             }
         }
     },
-    goToRoute: function (htmlName) {
-        (function(scope) { 
+    goToRoute: function (htmlName, isRequestSent) {
+        if (isRequestSent) {
+            return;
+        }
+        (function(scope) {
             var url = 'html/' + htmlName,
                 xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function () {
@@ -61,6 +67,7 @@ Router.prototype = {
             };
             xhttp.open('GET', url, true);
             xhttp.send();
+            isRequestSent = true;
         })(this);
     }
 };
