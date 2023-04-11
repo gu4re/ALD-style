e86c6731b1faae2f3766f8a8936a5bef9fba6a6b
angeel043 <cocybm@gmail.com>
2023-04-11 11:43:16 +0200

    Precios rebajados, html de shipping, boton de delete account, y varios fixes más

--- target/classes/static/js/app.js
+++ target/classes/static/js/app.js
@@ -16,7 +16,8 @@
             new Route('creditCard', 'creditCard.html'),
             new Route('forgotPasswd', 'forgotPasswd.html'),
             new Route('resetPasswd', 'resetPasswd.html'),
-            new Route('validate', 'login.html')
+            new Route('validate', 'login.html'),
+            new Route('shipping', 'shipping.html')
         ]);
     }
     init();
