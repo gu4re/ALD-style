ad00dcea37a80532c212193f332f6f84cda3a60b
Diego Picazo <99036688+gu4re@users.noreply.github.com>
2023-06-04 00:08:03 +0200

    Delete target directory

--- target/classes/static/js/filterCart.js
+++
@@ -1,7 +0,0 @@
-var filterButton = document.querySelector("#filter");
-filterButton.addEventListener("change", (event) => {
-  var script = document.createElement("script");
-  script.src = "js/showCart.js?" + Date.now();
-  document.head.appendChild(script);
-});
-
